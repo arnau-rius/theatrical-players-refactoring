7d60fe19766049037b80cc6d3e317e38a66eb2d4
arnau-rius <arnau.rius.contact@gmail.com>
2020-07-16 16:15:20 +0200

    export statement function as module

--- statement.js
+++ statement.js
@@ -12,39 +12,35 @@ function statement(invoice, plays) {
     const play = plays[perf.playID];
     let thisAmount = 0;
     switch (play.type) {
-      case "tragedy": {
+      case "tragedy":
         thisAmount = 40000;
         if (perf.audience > 30) {
           thisAmount += 1000 * (perf.audience - 30);
         }
         break;
-      }
-      case "comedy": {
+      case "comedy":
         thisAmount = 30000;
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 2);
+          thisAmount += 10000 + 500 * (perf.audience - 20);
         }
         thisAmount += 300 * perf.audience;
         break;
-      }
-      default: {
+      default:
         throw new Error(`unknown type: ${play.type}`);
-      }
     }
-
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
   }
-
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
+
+module.exports = statement;
