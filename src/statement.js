89e978fab8e1b3e7d067ffaf2bfa968c813a87ba
arnau-rius <arnau.rius.contact@gmail.com>
2020-07-20 13:19:33 +0200

    repleace play by playFor(aPerformance) in amountFor function

--- src/statement.js
+++ src/statement.js
@@ -30,9 +30,9 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -47,7 +47,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
